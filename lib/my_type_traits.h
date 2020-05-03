6621811c451ed7b9a0e3f5941a66743ba8f72afc
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2020-05-03 09:49:39 +0800

    Update my_type_traits.h

--- lib/my_type_traits.h
+++ lib/my_type_traits.h
@@ -176,7 +176,6 @@ namespace my_lib
     {
         typedef __true_type     _has_trivial_default_constructor;
         typedef __true_type     _has_trivial_copy_constructor;
-        typedef __true_type     _has_trivial_default_constructor;
         typedef __true_type     _has_trivial_assignment_operator;
         typedef __true_type     _has_trivial_destructor;
         typedef __true_type     _is_POD_type;
