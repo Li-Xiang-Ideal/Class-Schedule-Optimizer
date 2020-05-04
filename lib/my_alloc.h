8f022e4768a691dc77fb64fba888d5e64d381328
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2020-05-04 10:34:42 +0800

    Update my_alloc.h

--- lib/my_alloc.h
+++ lib/my_alloc.h
@@ -32,12 +32,13 @@
 #define MY_ALLOC
 
 #include <new>
+/*
 #include <cstddef>
 #include <cstdlib>
 #include <cstdio>
 #include <climits>
 #include <iostream>
-#include <memory>
+#include <memory> //*/
 #include <bits/c++config.h>
 #include <bits/functexcept.h>
 #include <bits/move.h>
@@ -74,51 +75,82 @@ namespace my_lib
     }
     
 #if __cplusplus >= 201103L
-    // construct的实现, 直接调用对象的构造函数
+    // construct的实现, 直接调用对象的无参构造函数
+    template<typename _Tp1>
+    inline void _construct(_Tp1* __p)
+	{ ::new((void *)__p) _Tp1(); }
+
+    // construct的实现, 直接调用对象的有参构造函数
     template<typename _Tp1, typename... _Vals>
-    inline void construct(_Tp1* __p, _Vals&&... __vals)
+    inline void _construct(_Tp1* __p, _Vals&&... __vals)
 	{ ::new((void *)__p) _Tp1(std::forward<_Vals>(__vals)...); }
 
     // destroy的简单实现, 直接调用对象的析构函数
     template <typename _Tp>
     inline void _destroy(_Tp* ptr) { ptr->~_Tp(); }
 
+
+    //*destroy的实现之一, 使用类模板参数类型推断
+
     // destroy的实现, 对象有非平凡析构函数之偏特化
-    template<typename _Tp>
+    template<bool>
     struct _destroy_aux
     {
         template<typename _ForwardIterator>
         static void __destroy(_ForwardIterator __first, _ForwardIterator __last) 
-        {
-	        for (; __first != __last; ++__first) { _destroy(_addressof(*__first)); }
-	    }
+        { for (; __first != __last; ++__first) { _destroy(_addressof(*__first)); } }
     };
 
     // destroy的实现, 对象没有非平凡析构函数之偏特化
     template<>
-    struct _destroy_aux<__true_type>
+    struct _destroy_aux<true>
     {
         template<typename _ForwardIterator>
         static void __destroy(_ForwardIterator, _ForwardIterator) { }
-    };
+    }; //注意这里用bool完成推断, 而非直接使用__true_type, 原因将在下文介绍*/
+
+
+    /*destroy的实现之二, 使用函数参数类型推断
+
+    // destroy的实现, 对象有非平凡析构函数之偏特化
+    template<typename _ForwardIterator>
+    void __destroy(_ForwardIterator __first, _ForwardIterator __last, __false_type) 
+    { for (; __first != __last; ++__first) { _destroy(_addressof(*__first)); } }
+
+    // destroy的实现, 对象没有非平凡析构函数之偏特化
+    template<typename _ForwardIterator>
+    void __destroy(_ForwardIterator __first, _ForwardIterator __last, __true_type) { } //*/
+    
 
     // destroy的实现, 识别对象是否有非平凡析构函数
     template<typename _ForwardIterator>
     inline void _destroy(_ForwardIterator _first, _ForwardIterator _last)
     {
         typedef typename iterator_traits<_ForwardIterator>::value_type _Value_type;
-        /*另一种实现, 直接生成临时_has_trivial_destructor, 此处不用
-        typedef typename type_traits<_Value_type>::_has_trivial_destructor _has_trivial_destructor;
-        _destroy_aux<_has_trivial_destructor()>::__destroy(_first, _last); //括号用于生成临时对象 */
-        //以下实现使用了type_traits中对_has_trivial_destructor的inline封装
-        _destroy_aux<_has_trivial_destructor(_Value_type)>::__destroy(_first, _last);
+        //以下实现使用了类模板参数类型推断
+        _destroy_aux<has_trivial_destructor<_Value_type>::value>::__destroy(_first, _last);
+        /*不能使用以下方法: 在_destroy_aux处使用__true_type推断, 而这里使用
+        _destroy_aux<has_trivial_destructor<_Value_type>>::__destroy(_first, _last);
+        原因在于编译器会在_destroy_aux<...>处直接推断为non-trivial, 
+        而不会先推断has_trivial_destructor<_Value_type>再推断_destroy_aux<...>*/
+
+        /*也不能使用以下方法: 用___has_trivial_destructor(_Value_type()).value推断, 
+        因为这是试图用函数返回值推断, 是无法进行的*/
+
+        /*另一种实现, 使用inline函数进行函数参数类型推断
+        __destroy(_first, _last, _has_trivial_destructor(_Value_type())); /* 括号用于生成临时对象 */
     }
 
 #else
-    // construct的实现, 直接调用对象的构造函数
-    template <typename _Tp1, typename _Val>
-    inline void _construct(_Tp1* __p, const _Val& __val) 
-    { new((void *)__p) _Tp1(__val); }
+    // construct的实现, 直接调用对象的无参构造函数
+    template<typename _Tp1>
+    inline void _construct(_Tp1* __p)
+	{ ::new((void *)__p) _Tp1(); }
+
+    // construct的实现, 直接调用对象的有参构造函数
+    template<typename _Tp1, typename... _Vals>
+    inline void _construct(_Tp1* __p, _Vals&&... __vals)
+	{ ::new((void *)__p) _Tp1(std::forward<_Vals>(__vals)...); }
 
     // destroy的实现, 直接调用对象的析构函数
     template <typename _Tp>
@@ -150,9 +182,9 @@ namespace my_lib
         { typedef allocator<_Tp1> other; };
 
 #if __cplusplus >= 201103L
-      // _GLIBCXX_RESOLVE_LIB_DEFECTS
-      // 2103. propagate_on_container_move_assignment
-      typedef std::true_type propagate_on_container_move_assignment;
+        // _GLIBCXX_RESOLVE_LIB_DEFECTS
+        // 2103. propagate_on_container_move_assignment
+        typedef std::true_type propagate_on_container_move_assignment;
 #endif
 
         // allocate, deallocate, construct和destroy函数均调用上面的实际实现
@@ -163,14 +195,18 @@ namespace my_lib
         void deallocate(pointer _p, size_type _n) { _deallocate(_p); }
 
 #if __cplusplus >= 201103L
-        template<typename _Tp1, typename... _Args>
-        void construct(pointer _p, const _Args&&... __args) { _construct(_p, _args); }
+        void construct(pointer _p) { _construct(_p); }
+
+        template<typename... _Args>
+        void construct(pointer _p, const _Args&&... __args) { _construct(_p, __args...); }
+
         void destroy(pointer _p) { _destroy(_p); }
 
         template<typename _ForwardIterator>
         void destroy(_ForwardIterator _first, _ForwardIterator _last) 
         { _destroy(_first, _last); }
 #else
+        void construct(pointer _p) { _construct(_p); }
         void construct(pointer _p, const _Tp& _Arg) { _construct(_p, _Arg); }
         void destroy(pointer _p) { _destroy(_p); }
 #endif
